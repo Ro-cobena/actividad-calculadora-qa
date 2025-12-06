86bc1b233222d82581950cbefd60915dac53bbdd
Ro-cobena <r.cobena@est.ecotec.edu.ec>
2025-12-06 13:09:58 -0500

    Rompiendo test a propósito

--- calculadora.py
+++ calculadora.py
@@ -10,3 +10,5 @@ class Calculadora:
             raise ValueError("División por cero no permitida")
         return a / b
 # Comentario de prueba para CI
+def test_suma(calc):
+    assert calc.suma(2, 3) == 6  # valor incorrecto a propósito
